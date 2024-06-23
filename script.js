74835916f5b8d07b859f7a3c026d59ca6f32176f
yuyasaito0105 <yuya.saito.wills@gmail.com>
2024-06-23 21:16:08 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+//これは知ってるやつ
+function tnok(){
+    alert("ボタン！");
+    const tdn = document.getElementById('tdn');
+    let yajyu = 114514
+    let mur = 364
+    
+    //let output = document.getElementById("output");
+    //output.innerHTML = "表示されました！";
+    //document.getElementById("fortuneResult").innerText = yajyu;
+}
+const btn1 = document.getElementById('btn1');
+btn1.addEventListener('click', () => {
+  alert('ボタン１がクリックされました！');
+});
